6ebdade32134c273203d85fe1fad9bb9dfb557d4
Karina <k_ale@outlook.com>
2019-12-20 11:01:35 -0300

    Agrupaciones de objetos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,12 +10,22 @@ export class DataComponent {
 
   forma: FormGroup;
 
+  usuario:Object = {
+    nombrecompleto: {
+      nombre: 'kari',
+      apellido: 'celis'
+    },
+    correo: 'kari@gmail.com'
+  };
+
   constructor() {
     this.forma = new FormGroup({
 
-      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'apellido': new FormControl('', Validators.required),
-      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required)
+      }),
+         'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
   }
 
