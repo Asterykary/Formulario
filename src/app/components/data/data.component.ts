ad1a5758f808d2f365e26cd3a0999be504666fa0
Karina <k_ale@outlook.com>
2019-12-20 12:52:34 -0300

    Termino

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
+
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
