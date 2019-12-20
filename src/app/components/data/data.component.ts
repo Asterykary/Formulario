5bc6327443a3ddc3920515e43da23151ea7aea3d
Karina <k_ale@outlook.com>
2019-12-20 12:26:17 -0300

    Validando que password2 sea igual a password1

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -24,35 +24,65 @@ export class DataComponent {
 
       'nombrecompleto': new FormGroup({
         'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-        'apellido': new FormControl('', Validators.required)
+        'apellido': new FormControl('', [Validators.required,
+                                          this.noCelis])
       }),
          'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
         ]),
          'pasatiempos': new FormArray([
            new FormControl('Correr', Validators.required)
-         ])
+         ]),
+         'password1': new FormControl('', Validators.required),
+         'password2': new FormControl()
     });
 
-    //this.forma.setValue(this.usuario);
+    // this.forma.setValue(this.usuario);
+
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind( this.forma )
+    ]);
   }
 
-  agregarPasatiempo(){
+  agregarPasatiempo() {
     (<FormArray>this.forma.controls['pasatiempos']).push(
       new FormControl('Dormir', Validators.required)
     )
   }
 
-  guardarCambios(){
+  noCelis(control: FormControl): {[s: string]: boolean} {
+    if (control.value === 'Celis') {
+      return {
+        noCelis: true
+      };
+    }
+
+    return null;
+  }
+
+  noIgual(control: FormControl): {[s: string]: boolean} {
+    const forma: any = this;
+
+    if (control.value !== forma.controls['password1'].value) {
+      return {
+        noiguales: true
+      };
+    }
+
+    return null;
+  }
+
+  guardarCambios() {
     console.log(this.forma.value);
     console.log(this.forma);
 
-    this.forma.reset({
+    /*this.forma.reset({
       nombrecompleto: {
         nombre: '',
         apellido: ''
       },
         correo: ''
-    });
+    });*/
   }
 
 
